5d7339efc69dbe5cc55ee25d06042a6ecee95eca
Pierre-Chanel <pierrechanel.gauthier@gmail.com>
2017-04-12 20:58:55 +0200

    Improve script by caching elements

--- fiddlestick.js
+++ fiddlestick.js
@@ -1,43 +1,61 @@
 var fiddlestick = (function () {
-    Array.prototype.slice.call(
-        document.querySelectorAll('[data-fiddlestick-id]')
-    )
-    .map(function (e) {
-        return e.getAttribute('data-fiddlestick-id');
-    })
-    .filter(function (value, index, self) { 
-        return self.indexOf(value) === index;
-    })
-    .forEach(function(fiddleId) {
-        update(fiddleId);
-        document.querySelectorAll(
-            '[data-fiddlestick-type=js], ' +
-            '[data-fiddlestick-type=html], ' +
-            '[data-fiddlestick-type=css]'
-        ).forEach(function (element) {
-            element.addEventListener('keyup', function (event) {
-                console.log('changed');
-                var fiddle = event.target || event.srcElement;
-                update(fiddle.getAttribute('data-fiddlestick-id'));
-            });
-            element.addEventListener('change', function (event) {
-                console.log('changed');
-                var fiddle = event.target || event.srcElement;
-                update(fiddle.getAttribute('data-fiddlestick-id'));
-            });
-        });
-    });
+   
+    function init() {
+        // Init fiddles mapping.
+        var data = {};
+        document
+            .querySelectorAll('[data-fiddlestick-id]')
+            .forEach(function(e) {
+                var fiddleId = e.getAttribute('data-fiddlestick-id');
+                var fiddleType = e.getAttribute('data-fiddlestick-type');
 
-    function update(fiddleId) {
-        var output = document.querySelector(
-            '[data-fiddlestick-id='+fiddleId+'][data-fiddlestick-type=render]'
-        );
+                if (!data.hasOwnProperty(fiddleId)) {
+                    data[fiddleId] = {};
+                }
+
+                if (!data[fiddleId].hasOwnProperty(fiddleType)) {
+                    data[fiddleId][fiddleType] = [];
+                }
+
+                if (fiddleType === 'render') {
+                    data[fiddleId].render = {
+                        'element': e,
+                        'type': fiddleType,
+                        'id': fiddleId,
+                    };
+                } else {
+                    data[fiddleId][fiddleType].push({
+                        'element': e,
+                        'type': fiddleType,
+                        'id': fiddleId,
+                    });
+                    e.addEventListener('keyup', function (event) {
+                        var fiddle = event.target || event.srcElement;
+                        update(data[fiddle.getAttribute('data-fiddlestick-id')]);
+                    });
+                    e.addEventListener('change', function (event) {
+                        var fiddle = event.target || event.srcElement;
+                        update(data[fiddle.getAttribute('data-fiddlestick-id')]);
+                    });
+                }
+            });
+
+        for(var fiddleId in data) { 
+            if (data.hasOwnProperty(fiddleId)) {
+                update(data[fiddleId]);
+            }
+        }
+    }
+
+    function update(fiddle) {
+        var iframe = document.createElement('iframe');
+        var output = fiddle.render.element;
         output.innerHTML = '';
-        output.appendChild(document.createElement('iframe'));
+        output.appendChild(iframe);
 
-        var cssCode = getAllFromLang(fiddleId, 'css');
-        var jsCode = getAllFromLang(fiddleId, 'js');
-        var htmlCode = getAllFromLang(fiddleId, 'html');
+        var cssCode = getAllFromLang(fiddle.css);
+        var jsCode = getAllFromLang(fiddle.js);
+        var htmlCode = getAllFromLang(fiddle.html);
 
         var layout = '<html><head><style>'
             + htmlDecode(cssCode)
@@ -47,28 +65,24 @@ var fiddlestick = (function () {
             + htmlDecode(jsCode)
             + '</script></body></html>';
 
-        var iframe = output.querySelector(
-            '[data-fiddlestick-id='+fiddleId+'] iframe'
-        );
         iframe.contentDocument.open();
         iframe.contentDocument.writeln(layout);
         iframe.contentDocument.close();
     }
 
-    function getAllFromLang(fiddleId, lang) {
-        return Array.prototype.slice.call(
-            document.querySelectorAll(
-                '[data-fiddlestick-id='+fiddleId+']'+
-                '[data-fiddlestick-type='+lang+']'
-            )
-        )
-        .reduce(function (string, e) {
-            if (isTextBox(e)) {
-                return string + e.value;
-            } else {
-                return string + e.innerHTML;
-            }
-        }, '');
+    function getAllFromLang(langElements) {
+        if (langElements === undefined) {
+            return '';
+        }
+
+        return langElements
+            .reduce(function (string, e) {
+                if (isTextBox(e.element)) {
+                    return string + e.element.value;
+                } else {
+                    return string + e.element.innerHTML;
+                }
+            }, '');
     }
 
     function htmlDecode(input) {
@@ -82,10 +96,12 @@ var fiddlestick = (function () {
         if (tagName === 'textarea') return true;
         if (tagName !== 'input') return false;
         var type = element.getAttribute('type').toLowerCase(),
-            // if any of these input types is not supported by a browser, it will behave as input type text.
-            inputTypes = ['text', 'password', 'number', 'email', 'tel', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
+        // if any of these input types is not supported by a browser, it will behave as input type text.
+        inputTypes = ['text', 'password', 'number', 'email', 'tel', 'url', 'search', 'date', 'datetime', 'datetime-local', 'time', 'month', 'week']
         return inputTypes.indexOf(type) >= 0;
     }
+
+    init();
 
     return {
         update: update,
